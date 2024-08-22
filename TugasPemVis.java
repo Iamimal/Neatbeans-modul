8c0f2cd2c34b4ad5df6e4101bfaf542d769f35d8
Ibnumalik <113578927+Iamimal@users.noreply.github.com>
2024-08-22 10:00:30 +0700

    Add files via upload

---
+++ TugasPemVis.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspemvis;
+
+/**
+ *
+ * @author HP Series
+ */
+public class TugasPemVis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Modul3 modul1=new Modul3();
+        modul1.setVisible(true);
+    }
+    
+}
